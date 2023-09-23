b34cb5d7e29a0430b5ba2ce2e5b76e3bbf03e01a
Armellectx <56161210+Armellectx@users.noreply.github.com>
2023-09-23 20:19:31 +0200

    Update script.js debug

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ const colorDisplay = document.getElementById("colorDisplay");
 
 // Tableau de couleurs
 const colors = ["#FF5733", "#33FF57", "#5733FF", "#FFFF33", "#33FFFF"];
-const texts = ["la", "vie", "est", "belle", "oui", "oui"]
+const texts = ["la", "vie", "est", "belle", "oui", "oui"];
 // Fonction pour changer la couleur au hasard
 function changeColor() {
     const randomIndex = Math.floor(Math.random() * colors.length);
     const randomColor = colors[randomIndex];
     colorDisplay.style.color = randomColor;
-    colorDisplay.textContent = texts[randomIndex]
+    colorDisplay.textContent = texts[randomIndex];
 }
 
 // Ajoutez un gestionnaire d'événements au bouton
