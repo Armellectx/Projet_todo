57df106ebd8f94a7f2f94cd2374254e6918cc3a5
Armellectx <56161210+Armellectx@users.noreply.github.com>
2023-09-23 19:37:07 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// Sélectionnez le bouton et le div de l'affichage de la couleur
+const changeColorButton = document.getElementById("changeColorButton");
+const colorDisplay = document.getElementById("colorDisplay");
+
+// Tableau de couleurs
+const colors = ["#FF5733", "#33FF57", "#5733FF", "#FFFF33", "#33FFFF"];
+
+// Fonction pour changer la couleur au hasard
+function changeColor() {
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    const randomColor = colors[randomIndex];
+    colorDisplay.style.color = randomColor;
+    colorDisplay.textContent = "Couleur : " + randomColor;
+}
+
+// Ajoutez un gestionnaire d'événements au bouton
+changeColorButton.addEventListener("click", changeColor);
