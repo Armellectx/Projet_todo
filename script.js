ed2408283473300295464747bb191ffc9be74ebf
Armellectx <56161210+Armellectx@users.noreply.github.com>
2023-09-23 20:26:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 // Sélectionnez le bouton et le div de l'affichage de la couleur
 const changeColorButton = document.getElementById("changeColorButton");
 const colorDisplay = document.getElementById("colorDisplay");
+const phrases = ["Thorben", "I", "miss", "you", "already", " :O", "What", "did", "you", "do", "to", "me", "I", "wish", "you", "shoot", "a", "fox", "Lol", "NO", "I", "am", "joking", "I", "am", "team", "Fox", "oupsi", "<3", "<333333", "kiss kiss"];
+let phraseIndex = 0;
 
 // Tableau de couleurs
 const colors = ["#FF5733", "#33FF57", "#5733FF", "#FFFF33", "#33FFFF"];
-const texts = ["la", "vie", "est", "belle", "oui", "oui"];
-// Fonction pour changer la couleur au hasard
-function changeColor() {
+
+// Fonction pour changer la couleur et afficher la phrase
+function changeColorAndPhrase() {
     const randomIndex = Math.floor(Math.random() * colors.length);
     const randomColor = colors[randomIndex];
     colorDisplay.style.color = randomColor;
-    colorDisplay.textContent = texts[randomIndex];
+    colorDisplay.textContent = phrases[phraseIndex];
+    phraseIndex = (phraseIndex + 1) % phrases.length;
 }
 
 // Ajoutez un gestionnaire d'événements au bouton
-changeColorButton.addEventListener("click", changeColor);
+changeColorButton.addEventListener("click", changeColorAndPhrase);
+
